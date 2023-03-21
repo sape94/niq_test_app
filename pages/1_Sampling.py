2e860d01f31f4b93f4cfaddf44fb35b3efbac68e
Sergio P <123118879+sape94@users.noreply.github.com>
2023-03-21 10:59:40 -0600

    Update 1_Sampling.py

--- pages/1_Sampling.py
+++ pages/1_Sampling.py
@@ -16,7 +16,7 @@ st.set_page_config(
 
 hide_menu_style = """
         <style>
-        #MainMenu {visibility: hidden;}
+        #MainMenu {visibility: display;}
         </style>
         """
 st.markdown(hide_menu_style, unsafe_allow_html=True)
@@ -44,7 +44,7 @@ selected = option_menu(
 )
 
 
-@st.cache
+@st.cache_data
 def sampled(df, sample_size):
     df = feas_df.copy()
     s_df = df.sample(n=sample_size)
@@ -166,66 +166,111 @@ if selected == 'Sampling':
             if samp_ans == 'Yes, with a non-stratified method':
 
                 sampled_df = sampled(uploaded_file, n)
-                if st.button(':inbox_tray: Press here to re-sample :inbox_tray:'):
-                    sampled_df = sampled_2(uploaded_file, n)
+                
+                coluno, colunos = st.columns(2, gap='medium')
+                
+                with coluno:
+                    if st.button(':inbox_tray: Press here to re-sample :inbox_tray:'):
+                        sampled_df = sampled_2(uploaded_file, n)
+                        
                 st.write(sampled_df)
                 sampled_df_csv = sampled_df.to_csv(index=False)
-                col5, col6 = st.columns(2, gap='medium')
-                with col5:
-                    sampled_df_2 = sampled_df[['Player',
-                                               'Subplayer', 'City', 'State']]
-                    sampled_df_3 = sampled_df[['SHO_ID']]
-                    filter_list = st.multiselect(
-                        'Select the columns (by priority) to get the structure', sampled_df_2.columns, max_selections=3)
-
-                    original_rows = feas_df.index.values.tolist()
-                    used_rows = sampled_df.index.values.tolist()
-                    unused_rows = [item for item in original_rows
-                                   if item not in used_rows]
-                    # ws_df stands for without sample dataframe.
-                    ws_df = feas_df.filter(items=unused_rows, axis=0)
-                    ws_df = ws_df.sort_values(by='ACV', ascending=False)
-
-                    rmv_list = st.multiselect(
-                        'Select the stores that you want to remove by SHO ID', sampled_df_3)
-                    col_n = list(sampled_df.columns.values)
-                    id_rmv = 'SHO_ID'
-                    # Removing the selected stores:
-                    rmv_s_df = sampled_df.copy()
-                    in_s_df = sampled_df.copy()
-                    # rmv_s_df stands for removed stores from sampled dataframe.
-                    rmv_s_df = rmv_s_df[rmv_s_df[id_rmv].isin(rmv_list)]
-                    # in_s_df stands for incomplete new sampled dataframe.
-                    in_s_df = in_s_df[~in_s_df[id_rmv].isin(rmv_list)]
-                    # Getting rmv_s_df characteristics:
-                    # stc_rmv_df stands for structure of removed items from the sampled dataframe.
-                    stc_rmv_df = rmv_s_df[filter_list]
-                    # stc_dic stands for stc_rmv_df dictionary.
-                    stc_dic = stc_rmv_df.to_dict('list')
-
-                    add_df_2 = pd.DataFrame(columns=col_n)
-                    l = len(filter_list)
-
-                    for row in range(0, rmv_s_df.shape[0]):
-                        temp_df = ws_df.copy()
-                        for i in range(l):
-                            temp_df = temp_df[temp_df[filter_list[i]]
-                                              == stc_dic[filter_list[i]][row]]
-                            if temp_df.empty:
-                                break
-                        if temp_df.empty:
-                            ta_df = ws_df.iloc[0]
-                        else:
-                            ta_df = temp_df.iloc[0]
-                        add_df_2 = pd.concat(
-                            [add_df_2, ta_df.to_frame().transpose()])
-                st.write(add_df_2)
-
-                with col6:
+                
+                coldoss, coldos = st.columns(2, gap='medium')
+                
+                with coldos:
                     st.download_button(label=':floppy_disk: Download Dataframe as CSV :floppy_disk:',
                                        data=sampled_df_csv,
                                        file_name=f'SAMPLED_{file_name_df}.csv',
                                        mime='text/csv')
+                
+                col5, col6 = st.columns(2, gap='medium')
+                
+                sampled_df_2 = sampled_df[['Player',
+                                            'Subplayer', 'City', 'State']]
+                sampled_df_3 = sampled_df[['SHO_ID']]
+                
+                filter_list = st.multiselect(
+                    'Select the columns (by priority) to get the structure', sampled_df_2.columns, max_selections=3)
+
+                original_rows = feas_df.index.values.tolist()
+                used_rows = sampled_df.index.values.tolist()
+                unused_rows = [item for item in original_rows
+                                if item not in used_rows]
+                # ws_df stands for without sample dataframe.
+                ws_df = feas_df.filter(items=unused_rows, axis=0)
+                ws_df = ws_df.sort_values(by='ACV', ascending=False)
+
+                rmv_ans = st.radio('Provide the stores by SHO_ID that you want to remove:',
+                                    ('Do it by selecting items from a list',
+                                    'Upload a Dataframe'))
+                if rmv_ans == 'Do it by selecting items from a list':
+                    rmv_list = st.multiselect(
+                        'Select the stores that you want to remove by SHO ID', sampled_df_3)
+                
+                if rmv_ans == 'Upload a Dataframe':
+                    up_rmv_file = st.file_uploader("Choose a file",
+                                        type=['csv'],
+                                        key='settings_df'
+                                        )
+                    if up_rmv_file is None:
+                        rmv_list = []
+                    if up_rmv_file is not None:
+                        to_rmv_df = pd.read_csv(up_rmv_file)
+                        file_name_df = up_rmv_file.name
+                        #st.write(feas_df)
+                        #st.write(to_rmv_df['SHO_ID'])
+                        rmv_list = to_rmv_df['SHO_ID'].to_list()
+                        
+                
+                col_n = list(sampled_df.columns.values)
+                id_rmv = 'SHO_ID'
+                # Removing the selected stores:
+                rmv_s_df = sampled_df.copy()
+                in_s_df = sampled_df.copy()
+                # rmv_s_df stands for removed stores from sampled dataframe.
+                rmv_s_df = rmv_s_df[rmv_s_df[id_rmv].isin(rmv_list)]
+                # in_s_df stands for incomplete new sampled dataframe.
+                in_s_df = in_s_df[~in_s_df[id_rmv].isin(rmv_list)]
+                # Getting rmv_s_df characteristics:
+                # stc_rmv_df stands for structure of removed items from the sampled dataframe.
+                stc_rmv_df = rmv_s_df[filter_list]
+                # stc_dic stands for stc_rmv_df dictionary.
+                stc_dic = stc_rmv_df.to_dict('list')
+
+                add_df = pd.DataFrame(columns=col_n)
+                l = len(filter_list)
+
+                for row in range(0, rmv_s_df.shape[0]):
+                    temp_df = ws_df.copy()
+                    for i in range(l):
+                        temp_df = temp_df[temp_df[filter_list[i]]
+                                            == stc_dic[filter_list[i]][row]]
+                        if temp_df.empty:
+                            break
+                    if temp_df.empty:
+                        ta_df = ws_df.iloc[0]
+                    else:
+                        ta_df = temp_df.iloc[0]
+                    add_df = pd.concat(
+                        [add_df, ta_df.to_frame().transpose()])
+                # Concatenating the incomplete new sampled dataframe and the rows to add
+                # from the unused ones preserving the structure of the removed ones into
+                # a new sampled dataframe.
+                n_s_df = pd.concat([in_s_df, add_df])
+                new_sampled_df_csv = n_s_df.to_csv(index=False)
+                
+                st.write(n_s_df)
+                
+                col7, col8 = st.columns(2, gap='medium')
+                    
+                with col8:
+                    st.download_button(label=':floppy_disk: Download Dataframe as CSV :floppy_disk:',
+                                        data=new_sampled_df_csv,
+                                        file_name=f'NEW_SAMPLED_{file_name_df}.csv',
+                                        mime='text/csv')
+
+                
 
             if samp_ans == 'Yes, with a stratified method':
                 st.write(':arrow_forward: This method is to be implemented soon.')
@@ -293,7 +338,7 @@ text-align: left; /* 'left', 'center' or 'right' if you want*/
 <div class="footer">
 <p style='font-size: 0.875em;'>Developed by <a style='display: inline; text-align:
 left;' href="https://github.com/sape94" target="_blank"><img src="https://i.postimg.cc/vBnHmZfF/innovation-logo.png"
-alt="AI" height= "20"/><br>LatAm's Automation & Innovation Team </br></a>.</p>
+alt="AI" height= "20"/><br>LatAm's Automation & Innovation Team.</br></a></p>
 </div>
 
 </div>
