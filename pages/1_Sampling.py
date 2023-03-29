369b4157793636d845749d4a96f2756ac9179409
Sergio P <123118879+sape94@users.noreply.github.com>
2023-03-29 12:07:34 -0600

    Update 1_Sampling.py

--- pages/1_Sampling.py
+++ pages/1_Sampling.py
@@ -4,6 +4,8 @@ from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
 import re
+from app_modules import sampling_module as samp_mod
+from app_modules import replacing_module as repl_mod
 
 # DO_NOT_CHANGE########################################################
 #######################################################################
