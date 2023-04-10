f68efda1add57f2493cc27a97caff46f3498c651
Sergio P <123118879+sape94@users.noreply.github.com>
2023-04-10 15:43:14 -0600

    Update 1_Sampling.py

--- pages/1_Sampling.py
+++ pages/1_Sampling.py
@@ -3,7 +3,6 @@ import pandas as pd
 from streamlit_option_menu import option_menu
 from streamlit_extras.switch_page_button import switch_page
 from PIL import Image
-import re
 from app_modules import sampling_module as samp_mod
 from app_modules import replacing_module as repl_mod
 
@@ -39,34 +38,49 @@ selected = option_menu(
         "container": {"padding": "0!important",
                       "background-color": "#fafafa"},
         "icon": {"color": "#31d1ff", "font-size": "15px"},
-        "nav-link": {"color": "#31333F", "font-size": "15px", "text-align": "centered",
+        "nav-link": {"color": "#31333F", "font-size": "15px",
+                     "text-align": "centered",
                      "margin": "0px", "--hover-color": "#eee"},
-        "nav-link-selected": {"color": "#FFFFFF", "background-color": "#090a47"},
+        "nav-link-selected": {"color": "#FFFFFF",
+                              "background-color": "#090a47"},
     }
 )
 
 
-@st.cache
-def cache_df(df, p, conf_lev, s_e):
-    c_df = samp_mod.SamplingMachine(sample_portion=p,
+@st.cache_data
+def cache_df(o_df, p, conf_lev, s_e):
+    cc_1 = samp_mod.SamplingMachine(sample_portion=p,
                                     confidence_level=conf_lev,
-                                    standard_error=s_e).rand_samp(df)
-    return c_df
+                                    standard_error=s_e).rand_samp(o_df)
+    return cc_1
 
 
-def cache_df_2(df, p, conf_lev, s_e):
-    df = df.copy()
-    df_2 = df.sample(frac=1)
-    c_df_2 = samp_mod.SamplingMachine(sample_portion=p,
-                                      confidence_level=conf_lev,
-                                      standard_error=s_e).rand_samp(df_2)
-    return c_df_2
+def cache_df_2(o_df, p, conf_lev, s_e):
+    cc_2 = samp_mod.SamplingMachine(sample_portion=p,
+                                    confidence_level=conf_lev,
+                                    standard_error=s_e).rand_samp(o_df)
+    return cc_2
 
 
 if selected == 'Home':
     switch_page('NIQ prototype')
 
 if selected == 'Sampling':
+    subhead_app_1 = '''
+    <style>
+    .subhead-item {
+        backgroundcolor: transparent;
+    }
+    .subhead-item:hover {
+        color: #2E6EF7;
+    }
+    </style>
+
+    <h3 class="subhead-item">
+    Sampling app
+    </h3>
+    '''
+    st.write(subhead_app_1, unsafe_allow_html=True)
 
     st.markdown('The **sample size\' formula** is the following:')
     st.latex(r'n = \frac{NZ^{2}pq}{e^{2}(N-1)+Z^{2}pq};')
@@ -172,14 +186,21 @@ if selected == 'Sampling':
                                  'Yes, with a parameters method'))
             if samp_ans == 'Yes, with a non-stratified method':
 
-                sampled_df = cache_df(df=o_df, p=p, conf_lev=conf_lev, s_e=s_e)
+                # sampled_df = samp_mod.SamplingMachine(sample_portion=p,
+                #                                      confidence_level=conf_lev,
+                #                                      standard_error=s_e).rand_samp(o_df)
+                sampled_df = cache_df(o_df, p=p, conf_lev=conf_lev, s_e=s_e)
 
-                coluno, colunos = st.columns(2, gap='medium')
+                # coluno, colunos = st.columns(2, gap='medium')
 
-                with coluno:
-                    if st.button(':inbox_tray: Press here to re-sample :inbox_tray:'):
-                        sampled_df = cache_df_2(
-                            df=o_df, p=p, conf_lev=conf_lev, s_e=s_e)
+                # with coluno:
+                if st.button(':inbox_tray: Press here to re-sample :inbox_tray:'):
+                    # sampled_df = samp_mod.SamplingMachine(sample_portion=p,
+                    #                                      confidence_level=conf_lev,
+                    #                                      standard_error=s_e).rand_samp(o_df.sample(frac=1))
+                    o_df = o_df.sample(frac=1)
+                    sampled_df = cache_df_2(
+                        o_df, p=p, conf_lev=conf_lev, s_e=s_e)
 
                 st.write(sampled_df)
                 sampled_df_csv = sampled_df.to_csv(index=False)
@@ -192,63 +213,32 @@ if selected == 'Sampling':
                                        file_name=f'SAMPLED_{file_name_df}.csv',
                                        mime='text/csv')
 
-                remove_ans = st.radio(
-                    'Do you want to remove stores from the sampled Dataframe?', ('No', 'Yes'))
+                st.write('')
+                st.write('Don\'t forget to **download** your sampled Dataframe.')
+                st.write(
+                    'If you want to remove stores from the sampled Dataframe use our:')
+                subhead_app_2 = '''
+                <style>
+                .subhead-item_2 {
+                    color: #2E6EF7;
+                    backgroundcolor: transparent;
+                }
+                .subhead-item_2:hover {
+                    color: #164fc9;
+                }
+                </style>
 
-                if remove_ans == 'Yes':
-
-                    col5, col6 = st.columns(2, gap='medium')
-
-                    est_col = ['Player', 'Subplayer', 'City', 'State']
-                    sort_col = 'ACV'
-                    id_rmv = 'SHO_ID'
-
-                    filter_list = st.multiselect(
-                        'Select up to three columns by priority to get the structure:', est_col, max_selections=3)
-                    original_rows = o_df.index.values.tolist()
-                    used_rows = sampled_df.index.values.tolist()
-                    unused_rows = [item for item in original_rows
-                                   if item not in used_rows]
-                    ws_df = o_df.filter(items=unused_rows, axis=0)
-                    ws_df = ws_df.sort_values(by='ACV', ascending=False)
-
-                    rmv_ans = st.radio('Provide the stores by SHO_ID that you want to remove:',
-                                       ('Do it by selecting items from a list',
-                                        'Upload a Dataframe'))
-                    if rmv_ans == 'Do it by selecting items from a list':
-                        rmv_list = st.multiselect(
-                            f'Select the stores that you want to remove by {id_rmv}', sampled_df[[f'{id_rmv}']])
-
-                    if rmv_ans == 'Upload a Dataframe':
-                        up_rmv_file = st.file_uploader("Choose a file",
-                                                       type=['csv', 'xlsx'],
-                                                       key='settings_df'
-                                                       )
-
-                        if up_rmv_file is not None:
-                            to_rmv_df = pd.read_csv(up_rmv_file)
-                            file_name_df = up_rmv_file.name
-                            rmv_list = to_rmv_df['SHO_ID'].to_list()
-                        else:
-                            rmv_list = []
-
-                    n_s_df = repl_mod.DataFrameReplacer(fulldf=o_df,
-                                                        fracdf=sampled_df,
-                                                        sort_col=sort_col).add_sts(est_col=filter_list,
-                                                                                   rmv_list=rmv_list,
-                                                                                   id_rmv=id_rmv)
-
-                    new_sampled_df_csv = n_s_df.to_csv(index=False)
-
-                    st.write(n_s_df)
-
-                    col7, col8 = st.columns(2, gap='medium')
-
-                    with col8:
-                        st.download_button(label=':floppy_disk: Download Dataframe as CSV :floppy_disk:',
-                                           data=new_sampled_df_csv,
-                                           file_name=f'NEW_SAMPLED_{file_name_df}.csv',
-                                           mime='text/csv')
+                <a style='display: inline; text-align: center; color: #31333F
+                ; text-decoration: none; '
+                href="/Replacing" target="_self">
+                <h5 class="subhead-item_2">
+                Replacing app
+                </h5>
+                </a>
+                '''
+                st.write(subhead_app_2, unsafe_allow_html=True)
+                st.write('')
+                st.write('')
 
             if samp_ans == 'Yes, with a stratified method':
                 st.write(':arrow_forward: This method is to be implemented soon.')
@@ -267,6 +257,7 @@ if selected == 'Sampling':
             st.markdown(
                 r'- $\textrm{w}_{\textrm{G}}(\tau)$ is the **normal** probability density function,')
             st.write(r'- $\alpha\in[0,1]$, the **confidence level**.')
+            st.write('')
 
 
 if selected == 'Feasibilities':
